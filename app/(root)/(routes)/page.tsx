6922fcf153cacfe3ffc20a3c8357e54e64dc9312
Abdul Lahad <lahad@example.com>
2025-08-28 17:36:08 +0500

    add(List companion cards)

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,52 @@
 import { Categories } from "@/components/categories";
 import prismadb from "@/lib/prisma";
 import { SearchInput } from "@/components/searchInput";
-import { UserButton } from "@clerk/nextjs";
-const RootPage = async () => {
+import { CompanionPage } from "@/components/companions";
+
+
+interface RootPageProps{
+   searchParams:{
+    categoryId: string;
+    name: string;
+   } 
+}
+
+const RootPage = async ({searchParams}:RootPageProps) => {
+
+    const data = await prismadb.companion.findMany({
+        where:{
+            ...( searchParams.categoryId && {
+                categoryId: Number(searchParams.categoryId)
+            }),
+            
+        ...( searchParams.name && {
+            name:{
+                search: searchParams.name
+            }
+        })
+
+        },
+        orderBy:{
+            createdAt: "desc"
+        },
+        include:{
+            _count:{
+                select:{
+                    messages: true
+                }
+            }
+        }
+    }
+    )
+    
     const categories = await prismadb.category.findMany();
+    
     return (
         <div className="h-full p-4 space-y-2">
         <SearchInput/>
         <Categories data={categories}/>
-        
+        <CompanionPage data={data}/>
+
         </div>
     );
 }
